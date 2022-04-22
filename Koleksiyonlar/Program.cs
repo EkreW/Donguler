43cdc1941c562ed4ff0bda089768d640072d75e4
Ekrem Güngör <ekrmdsgnr@gmail.com>
2022-04-22 03:00:07 +0300

    trying

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[]
+            //{
+            //    "Ekrem", "İlker", "Mustafa", "Murat"
+            //};
+            //Console.WriteLine(isimler[0]);
+            ////Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+            //isimler[4] = "Alid";
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]);
+
+            List<string> isimler2 = new List<string> { "Ekrem", "İlker", "Mustafa", "Murat" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("Alid");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product prodcut1 = new Product();
+            prodcut1.Id = 1;
+            prodcut1.CategoryId = 2;
+            prodcut1.ProductName = "Masa";
+            prodcut1.UnitPrice = 500;
+            prodcut1.UnitsInStock = 3;
+
+            Product product2 = new Product { Id = 2, CategoryId = 5, UnitsInStock = 5, 
+                ProductName = "Kalem", UnitPrice = 35 };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(prodcut1);
+            Console.WriteLine(prodcut1.ProductName);
+
+        }
+    }
+}
